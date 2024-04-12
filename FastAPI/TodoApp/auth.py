78df1d7e9e832057471b091e79895b9045cef657
wjkba <63542318+wjkba@users.noreply.github.com>
2024-04-12 16:15:16 +0200

    update

--- FastAPI/TodoApp/auth.py
+++ FastAPI/TodoApp/auth.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from typing import Optional
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
 import models
+
+SECRET_KEY = "205x5K6NiUTOp92wTnnbxX6AL617LjKtbofuZXVegg0niaFG8xpmoBMwjTmzTF4f"
+ALGORITHM = "HS256"
+
 
 class CreateUser(BaseModel):
   username: str
@@ -23,6 +29,9 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # to stworzy database i wszystkie potrzebne rzeczy do tabelki,
 # jesli z jakiegos powodu auth.py zostanie uruchomione przed main.py
 models.Base.metadata.create_all(bind=engine)
+
+# uzycie dependency ktora bedzie ekstraktowac data z authorization header
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
@@ -49,6 +58,30 @@ def authenticate_user(username: str, password: str, db):
     return False
   return user
 
+
+# TWORZENIE TOKENA JWT
+def create_access_token(username: str, user_id: int, expires_delta: Optional[timedelta]= None):
+  encode = {"sub": username, "id": user_id}
+  if expires_delta:
+    expire = datetime.utcnow() + expires_delta
+  else:
+    expire = datetime.utcnow() + timedelta(minutes=15)
+  encode.update({"exp": expire})
+  return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: str = Depends(oauth2_bearer)):
+  try:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    username: str = payload.get("sub")
+    user_id: int = payload.get("id")
+    if username is None or user_id is None:
+      raise get_user_exception()
+    return {"username": username, "id": user_id}
+  except JWTError:
+    raise get_user_exception()
+
+
+
 @app.post("/create/user")
 async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):  
   create_user_model = models.Users()
@@ -70,5 +103,18 @@ async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
   user = authenticate_user(form_data.username, form_data.password, db)
   if not user:
-    raise HTTPException(status_code=404, detail="User not found")
-  return "User validated"
+    raise token_exception()
+  token_expires = timedelta(minutes=20)
+  token = create_access_token(user.username, user.id, expires_delta=token_expires)
+  return {"token": token}
+
+
+
+#Exceptions
+def get_user_exception():
+  credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+  return credentials_exception
+
+def token_exception():
+  token_exception_response = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"})
+  return token_exception_response
